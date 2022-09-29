16d48dd04335a1aa8212b4db380a0ebbe1b7393a
Administrator <admin@example.com>
2022-09-29 21:00:22 +0000

    Update mobility_was.py

--- Minimal/mobility_was.py
+++ Minimal/mobility_was.py
@@ -2,6 +2,16 @@ from flask import Flask
 import socket
 hostname=socket.gethostname()
 app = Flask(__name__)
+@app.route("/")
+def index():
+    return ('%s' %(str(hostname)))
+if __name__ == "__main__":
+    app.run(port=8888)
+'''
+from flask import Flask
+import socket
+hostname=socket.gethostname()
+app = Flask(__name__)
 ​
 ​
 @app.route("/")
@@ -11,3 +21,4 @@ def index():
 ​
 if __name__ == "__main__":
         app.run(port=8080)
+'''
