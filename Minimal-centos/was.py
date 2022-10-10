678bc30d0cf4e0cf4498d78f1b9b90dc84326c1f
Administrator <admin@example.com>
2022-10-10 12:00:48 +0000

    Add new file

---
+++ Minimal-centos/was.py
@@ -0,0 +1,24 @@
+from flask import Flask
+import socket
+hostname=socket.gethostname()
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return ('%s' %(str(hostname)))
+if __name__ == "__main__":
+    app.run(port=80)
+'''
+from flask import Flask
+import socket
+hostname=socket.gethostname()
+app = Flask(__name__)
+​
+​
+@app.route("/")
+def index():
+        return ('%s' %(str(hostname)))
+​
+​
+if __name__ == "__main__":
+        app.run(port=80)
+'''
